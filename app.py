108499efa02995af053c8f6e31b7e196ab07e21e
Laura Lasso <lauralassoporto@gmail.com>
2024-06-09 22:32:38 +0200

    camara x3

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from gesture_recognition import recognize_gestures
 
 app = Flask(__name__)
 
-
 @app.route('/video_feed')
 def video_feed():
-    print("HOLAAAA")
     return Response(recognize_gestures(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     app.run(host='0.0.0.0', port=5000)
